fa5ee09fbe14e6cad99ce3b511766d09f4e0d54b
yassine <yassine.azaiez@proxym-it.com>
2021-05-21 14:22:49 +0100

    book creations steps adpater

--- app/src/main/java/com/example/booksdonationapp/presentation/ui/createBook/adapter/BookCreationAdapter.kt
+++ app/src/main/java/com/example/booksdonationapp/presentation/ui/createBook/adapter/BookCreationAdapter.kt
@@ -1,4 +1,14 @@
 package com.example.booksdonationapp.presentation.ui.createBook.adapter
 
-class BookCreationAdapter {
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentActivity
+import androidx.viewpager2.adapter.FragmentStateAdapter
+
+class BookCreationAdapter(private val steps: List<Fragment>, context: FragmentActivity) :
+    FragmentStateAdapter(context) {
+    override fun getItemCount(): Int {
+        return steps.size
+    }
+
+    override fun createFragment(position: Int) = steps[position]
 }
